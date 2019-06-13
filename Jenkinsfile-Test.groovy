622e5cee6ac80b455ad898ea9919c6c5768b810c
Alex Lanz <alex@krumer.it>
2019-06-13 08:29:53 +0200

    Combining build and deploy

--- Jenkinsfile-Test.groovy
+++ Jenkinsfile-Test.groovy
@@ -9,7 +9,7 @@ pipeline {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
         DOCKER_IMAGE_APP = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/noi-maps"
-        DOCKER_TAG_APP = "$TAG"
+        DOCKER_TAG_APP = "test-$BUILD_NUMBER"
         DOCKER_SERVICES = "app"
         DOCKER_SERVER_IP = "63.33.73.203"
         DOCKER_SERVER_DIRECTORY = "/var/docker/noi-maps"
@@ -21,6 +21,13 @@ pipeline {
                 sh "echo '' > .env"
                 sh "echo 'DOCKER_IMAGE_APP=${DOCKER_IMAGE_APP}' >> .env"
                 sh "echo 'DOCKER_TAG_APP=${DOCKER_TAG_APP}' >> .env"
+            }
+        }
+        stage('Build & Push') {
+            steps {
+                sh "aws ecr get-login --region eu-west-1 --no-include-email | bash"
+                sh "docker-compose -f docker-compose.yml -f docker-compose.build.yml build ${DOCKER_SERVICES}"
+                sh "docker-compose -f docker-compose.yml -f docker-compose.build.yml push ${DOCKER_SERVICES}"
             }
         }
         stage('Deploy') {
