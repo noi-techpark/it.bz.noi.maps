ccd2d4019eb698abadf2780ecd2e5e2886be5bef
Alex Lanz <alex@krumer.it>
2020-01-07 08:55:54 +0100

    Adjusting project name strategy for Docker

--- Jenkinsfile-Test.groovy
+++ Jenkinsfile-Test.groovy
@@ -4,22 +4,21 @@ pipeline {
     environment {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+        DOCKER_PROJECT_NAME = "noi-maps"
         DOCKER_IMAGE_APP = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/noi-maps"
         DOCKER_TAG_APP = "test-$BUILD_NUMBER"
         DOCKER_SERVICES = "app"
         DOCKER_SERVER_IP = "63.33.73.203"
         DOCKER_SERVER_DIRECTORY = "/var/docker/noi-maps"
-        DOCKER_SERVER_PROJECT = "noi-maps"
     }
 
     stages {
         stage('Configure') {
             steps {
                 sh "echo '' > .env"
+                sh "echo 'COMPOSE_PROJECT_NAME=${DOCKER_PROJECT_NAME}' >> .env"
                 sh "echo 'DOCKER_IMAGE_APP=${DOCKER_IMAGE_APP}' >> .env"
                 sh "echo 'DOCKER_TAG_APP=${DOCKER_TAG_APP}' >> .env"
-                //sh '''sed -i -e "s%\\('sheet_url'\\ =>\\ \\).*\\$%\\1'https://docs.google.com/spreadsheets/d/e/2PACX-1vQA-8MjYhZBkN5ZIv_IPS2cUMvVTmoG0zCoSZMFLT8b_vo9qBfRO4lNwI33D8Z9xeF7gz7eZxE7_f74/pub?gid=0\\&output=tsv\\&range=A1:Z\\&single=true'%g" config.php'''
-                //sh '''sed -i -e "s%\\(\\$buildings_summary_sheet_url\\ =\\ \\).*\\$%\\1'https://docs.google.com/spreadsheets/d/e/2PACX-1vQA-8MjYhZBkN5ZIv_IPS2cUMvVTmoG0zCoSZMFLT8b_vo9qBfRO4lNwI33D8Z9xeF7gz7eZxE7_f74/pub?gid=322609536\\&output=tsv\\&single=true\\&range=A1:Z99'\\.rand(1,1000);%g" config.php'''
             }
         }
         stage('Build & Push') {
@@ -39,11 +38,11 @@ pipeline {
                     sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'mkdir -p ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}'"
                     sh "pv docker-compose.run.yml | ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'tee ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}/docker-compose.yml'"
                     sh "pv .env | ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'tee ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}/.env'"
-                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} && docker-compose --project-name=${DOCKER_SERVER_PROJECT} pull'"
+                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} && docker-compose pull'"
 
-                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} '[ -d \"${DOCKER_SERVER_DIRECTORY}/current\" ] && (cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose --project-name=${DOCKER_SERVER_PROJECT} down) || true'"
+                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} '[ -d \"${DOCKER_SERVER_DIRECTORY}/current\" ] && (cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose down) || true'"
                     sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'ln -sfn ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} ${DOCKER_SERVER_DIRECTORY}/current'"
-                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose --project-name=${DOCKER_SERVER_PROJECT} up --detach'"
+                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose up --detach'"
                 }
             }
 	    }
