d276510d2b374db88354f87874cb6744183ed30a
bertolla <p.bertolla@noi.bz.it>
2021-07-15 11:58:17 +0200

    add access keys

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -2,6 +2,8 @@ pipeline {
     agent any
 
     environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
         // FIXME: This is a workaround to solve a bug in the CDN which does not give the real latest version, if it does not follow semver schemes
         WEBCOMPONENT = "https://cdn.webcomponents.opendatahub.testingmachine.eu/dist/9232cce6-2f39-11eb-adc1-0242ac120002/development-16-20210712T1509/noi_maps_widget.min.js"
     }
